0a24f4b9d49b71154cb2a22655f5435647db0624
ASIA\ryan.delrosario <ryan.delrosario@ext.essilor.com>
2023-04-12 16:40:44 +0800

    add style and create a form

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
+import Postform from './postform/postform';
 
 export function App() {
   return (
-    <>
-      Ryan James
-    </>
+    <Postform />
   );
 }
 
--- src/app/postform/postform.tsx
+++ src/app/postform/postform.tsx
@@ -1,12 +1,46 @@
 import styles from './postform.module.scss';
-
+import { useState } from 'react';
 /* eslint-disable-next-line */
-export interface PostformProps {}
+export interface PostformProps { }
 
 export function Postform(props: PostformProps) {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<EventTarget>): void => {
+    e.preventDefault()
+    console.log('Post Title:', title)
+    console.log('Post Content:', content)
+  }
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Postform!</h1>
+      <section>
+        <h2>Add a New Post</h2>
+        <form>
+          <div>
+            <label htmlFor="username">Title</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Content</label>
+            <textarea
+              name="password"
+              id="password"
+              cols="30"
+              rows="10"
+              value={content}
+              onChange={e => setContent(e.target.value)}
+            ></textarea>
+          </div>
+          <button onClick={handleSubmit}>Add Post</button>
+        </form>
+      </section>
     </div>
   );
 }
